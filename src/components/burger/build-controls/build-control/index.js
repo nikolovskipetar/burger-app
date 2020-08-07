e681ea7e6d6133b56e4fc7726392484b1c4b16a7
nikolovskipetar <dw-petar@hotmail.com>
2020-08-07 11:19:06 +0200

    changed button names

--- src/components/burger/build-controls/build-control/index.js
+++ src/components/burger/build-controls/build-control/index.js
@@ -4,8 +4,8 @@ import classes from './index.css'
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button disabled={props.disabled} className={classes.Less} onClick={props.removed}>Less</button>
-    <button className={classes.More} onClick={props.added}>More</button>
+    <button disabled={props.disabled} className={classes.Less} onClick={props.removed}>Remove</button>
+    <button className={classes.More} onClick={props.added}>Add</button>
   </div>
 )
 
